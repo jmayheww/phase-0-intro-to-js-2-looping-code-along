a7895151a843272dffeab276231bedd83f50fbf7
Joshua <joshsmayhew@gmail.com>
2022-06-15 13:27:04 +0700

    commit tree a8fc5a70a557862dfb53a966ef4659f47cb1a2ef

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
-// Code your solutions in this file
+// // Code your solutions in this file
+// for(let age = 30; age < 40; age++) {
+
+//     console.log(`I'm ${age} years old. Happy Birthday to me!`);
+//     debugger;
+// }
+
+// const gifts = ['teddy bear', 'drone', 'doll'];
+
+// function wrapGifts(gifts) {
+//     for(let i = 0; i < gifts.length; i++) {
+//         console.log(`Wrapped ${gifts[i]} and added a bow!`);
+
+//     }
+
+//     return gifts;
+// }
+
+// wrapGifts(gifts);
+
+function writeCards(array, eventName) {
+
+    const messages = [];
+
+    for(let i = 0; i < array.length; i++) {
+
+        const message = `Thank you, ${array[i]}, for the wonderful ${eventName} gift!`;
+
+
+        messages.push(message);
+
+
+
+    }
+
+    return messages;
+
+
+
+}
+
+console.log(writeCards(['Guadalupe', 'Ollie', 'Aki'], 'surprise'));
+
+
+function countDown() {
+    let num = 10;
+   while (num >= 0) {
+    console.log(num--);
+   }
+
+
+
+}
+
+countDown();
+
